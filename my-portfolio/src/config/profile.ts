2499dbbb2713236ff63a8b04720bf892f728a57d
Mia <miaskyelena@gmail.com>
2025-05-14 12:16:28 -0400

    Updated resume

--- my-portfolio/src/config/profile.ts
+++ my-portfolio/src/config/profile.ts
@@ -9,7 +9,7 @@ export const profile: Profile = {
     links: {
         github: "https://github.com/miasdk",
         linkedIn: "https://www.linkedin.com/in/miaelena/",
-        resume: "https://www.dropbox.com/scl/fi/apvvtjqsnv2via15zvufq/Mia-Elena-Tapia-SWE-Resume.pdf?rlkey=63sdhal4calzoax41jmsinc0e&st=a6z7ik24&dl=0",
+        resume: "https://www.dropbox.com/scl/fi/apvvtjqsnv2via15zvufq/Mia-Elena-Tapia-SWE-Resume.pdf?rlkey=63sdhal4calzoax41jmsinc0e&st=3kvd66v4&dl=0",
     },
     skills: [
         "typescript",
