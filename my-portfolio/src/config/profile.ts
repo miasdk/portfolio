caa1e50b909fc6250dfb23194c471bbf67371d06
Mia Elena <miatapiaswe@gmail.com>
2025-07-03 21:31:28 -0400

    Edit profile

--- my-portfolio/src/config/profile.ts
+++ my-portfolio/src/config/profile.ts
@@ -1,11 +1,10 @@
 import { Profile, Technology } from "../types/index";
-import profilePicture from "../public/images/profile_transparent.jpg";
-import profilePicture2 from "../public/images/profile_transparent1.jpg";
+
 export const profile: Profile = {
     name: "Mia Elena",
     title: "Software Engineer",
     description: "Passionate about building impactful software solutions.",
-    image: profilePicture,
+    image: "/images/profile_transparent.jpg",
     links: {
         github: "https://github.com/miasdk",
         linkedIn: "https://www.linkedin.com/in/miaelena/",
