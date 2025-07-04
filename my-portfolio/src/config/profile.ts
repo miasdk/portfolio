083fba4fffd9ded901485c976e90c57e01a22d6f
Mia Elena <miatapiaswe@gmail.com>
2025-07-03 21:16:21 -0400

    Updated resume and skills

--- my-portfolio/src/config/profile.ts
+++ my-portfolio/src/config/profile.ts
@@ -9,18 +9,15 @@ export const profile: Profile = {
     links: {
         github: "https://github.com/miasdk",
         linkedIn: "https://www.linkedin.com/in/miaelena/",
-        resume: "https://www.dropbox.com/scl/fi/1yivfsqvvqfk5j1zbwc47/Mia-Elena-Tapia-SWE-Resume-2.pdf?rlkey=itpd6r6gmrlruh06v03cmx9f1&st=n7e6xd7y&dl=0",
+        resume: "https://www.dropbox.com/scl/fi/1yivfsqvvqfk5j1zbwc47/Mia-Elena-Tapia-SWE-Resume-2.pdf?rlkey=itpd6r6gmrlruh06v03cmx9f1&st=hsa672w8&dl=0",
     },
     skills: [
         "python",
         "django",
-        "postgresql",
         "react",
         "typescript",
-        "nextjs",
-        "docker",
-        "tailwindcss",
-        "awslambda"
+        "postgresql",
+        "aws"
     ] as Technology[],
     contact: {
         email: "miatapiaswe@gmail.com",
